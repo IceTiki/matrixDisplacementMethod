8738c18a93d4553ae826ee16a3b9e7bc3e3ab0bd
IceTiki <tiki_pufferfish@outlook.com>
2022-05-17 22:47:10 +0800

    更新示例

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 from matrixDisplacementMethod import Node, Element, Struction
 
-n1 = Node(position=(0, 0), lock=(1, 1, 1))
+n1 = Node(position=(0, 0), constraints=(1, 1, 1))
 n2 = Node(position=(2, 0))
 n3 = Node(position=(2, 1), load=(0, 0, 10))
-n4 = Node(position=(3, 1), lock=(1, 1, 1))
+n4 = Node(position=(3, 1), constraints=(1, 1, 1))
 e1 = Element(node=(n1, n2), q=-100)
 e2 = Element(node=(n2, n3))
 e3 = Element(node=(n3, n4))
